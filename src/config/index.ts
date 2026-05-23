97e84e7a762fc75b1bc1a7b5954f24b2069d3231
eh-munna <emran.h.munna@gmail.com>
2026-05-23 17:08:32 +0200

    conceptual part preparing...

--- src/config/index.ts
+++
@@ -1,7 +0,0 @@
-// ... existing code ...
-
-export const checkFunction = () => {
-  console.log('Check function executed');
-};
-
-// ... existing code ...
--- src/types/product.type.ts
+++
@@ -1,7 +0,0 @@
-export interface IProduct {
-  id: number;
-  name: string;
-  category: string;
-  price: number;
-  inStock: boolean;
-}
